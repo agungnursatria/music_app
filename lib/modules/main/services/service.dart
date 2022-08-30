1835f184cfe8f3cc9133e665365529b413a51306
agungnursatria <agungnursatria@gmail.com>
2022-08-30 16:04:22 +0700

    Create main service

---
+++ lib/modules/main/services/service.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:music_app/config/network/error/network_error.dart';
+import 'package:music_app/config/network/http/network_http_interface.dart';
+import 'package:music_app/config/network/http/network_http_model.dart';
+
+class MainService {
+  NetworkHttpInterface network;
+  MainService({required this.network});
+
+  Future<Either<NetworkModel, NetworkError>> getMusic(String? query) async {
+    return await network.requestGetNoAuth(
+      path: '/search',
+      queryParameters: {
+        "term": query,
+        "entity": "musicTrack",
+        "attribute": "artistTerm",
+      },
+    );
+  }
+}
