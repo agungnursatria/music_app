ca1083a92cf88026978db09e7e30124d30dcadb1
agungnursatria <agungnursatria@gmail.com>
2022-08-31 08:00:17 +0700

    Add multidex config to android native

--- android/app/src/main/kotlin/com/example/music_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/music_app/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.music_app
 
+import android.content.Context
+import androidx.multidex.MultiDex
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        MultiDex.install(this)
+    }
 }
